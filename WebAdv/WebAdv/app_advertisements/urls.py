2aaee31c9d34e1e36eff323df36be3a53a999a45
Maks <mammister20@gmail.com>
2023-07-10 17:40:50 +0300

    WebAdv

---
+++ WebAdv/WebAdv/app_advertisements/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import index
+urlpatterns=[
+    path('http://127.0.0.1/lesson_4',index)
+]
---
+++ WebAdv/WebAdv/app_advertisements/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from  django.http import HttpResponse
+def index(requst):
+    return HttpResponse('Успешно!')
