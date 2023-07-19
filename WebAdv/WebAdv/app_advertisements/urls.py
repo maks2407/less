7480042a4d068601fa5b93fdc8c574497f96ff4b
Maks <mammister20@gmail.com>
2023-07-19 18:17:17 +0300

    WebAdv

--- WebAdv/WebAdv/app_advertisements/urls.py
+++ WebAdv/WebAdv/app_advertisements/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import index
-urlpatterns=[
-    path('http://127.0.0.1/lesson_4',index)
+from .views import index, top_sellers
+
+urlpatterns = [
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
 ]
